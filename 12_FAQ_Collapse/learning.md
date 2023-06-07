7fa9cf13433e92ef081b4f7c00912d0d6cae8bff
Bevan Lewis <blew0009@student.monash.edu>
2023-06-07 21:51:17 +1000

    Finished exercise 12

---
+++ 12_FAQ_Collapse/learning.md
@@ -0,0 +1,10 @@
+HTML
+Learned how to structure files and name them.
+
+JS
+Learned how to get the parent element for a slected element.
+
+CSS
+Learned css chaining
+
+Learend how to combine css and js to make a panel that can appear or dissapear.
---
+++ 12_FAQ_Collapse/script.js
@@ -0,0 +1,9 @@
+const toggles = document.querySelectorAll(".faq-toggle");
+// Find all the toggles
+
+toggles.forEach((toggle) => {
+    // Add an event listener to all the toggles which gives its parent the active class.
+  toggle.addEventListener("click", () => {
+    toggle.parentNode.classList.toggle("active");
+  });
+});
