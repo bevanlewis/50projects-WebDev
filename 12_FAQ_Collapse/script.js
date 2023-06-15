bd357e15700dcff22dd5bf9538bf956907413cc4
Bevan Lewis <blew0009@student.monash.edu>
2023-06-15 23:06:04 +1000

    standardized styling

--- 12_FAQ_Collapse/script.js
+++ 12_FAQ_Collapse/script.js
@@ -2,7 +2,7 @@ const toggles = document.querySelectorAll(".faq-toggle");
 // Find all the toggles
 
 toggles.forEach((toggle) => {
-    // Add an event listener to all the toggles which gives its parent the active class.
+  // Add an event listener to all the toggles which gives its parent the active class.
   toggle.addEventListener("click", () => {
     toggle.parentNode.classList.toggle("active");
   });
--- 1_expanding_cards/script.js
+++ 1_expanding_cards/script.js
@@ -1,18 +1,18 @@
-const panels = document.querySelectorAll('.panel') 
+const panels = document.querySelectorAll(".panel");
 // Selects all the panels
 
 panels.forEach((panel) => {
-    // Checks for a click and then adds an active class to that panel
-    // All panels will have the eventlistener added to them
-    panel.addEventListener('click', () => {
-        removeActiveClasses()
-        panel.classList.add('active')
-    })
-})
+  // Checks for a click and then adds an active class to that panel
+  // All panels will have the eventlistener added to them
+  panel.addEventListener("click", () => {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
 
 // removes the active class from the panels
 function removeActiveClasses() {
-    panels.forEach(panel => {
-        panel.classList.remove('active')
-    })
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
 }
